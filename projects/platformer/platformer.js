91dbc459949bcaed818c3cf90861b265185c4e17
jaimejolee <68039146+jaimejolee@users.noreply.github.com>
2024-02-05 20:34:11 +0000

    createPlatforms

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -44,6 +44,10 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
+    createPlatform(100, 400, 400, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
+    createPlatform(700, 625, 600, 20);
+    createPlatform(100, 200, 350, 20);
+    createPlatform(425, 500, 600, 20);
 
 
     
